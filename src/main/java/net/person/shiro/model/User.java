6b2c0524cfd645f2175ce29fae8efc8357e4a1ef
lidekai <2665252078@qq.com>
2018-06-20 19:30:27 +0800

    创建项目

---
+++ src/main/java/net/person/shiro/model/User.java
@@ -0,0 +1,25 @@
+package net.person.shiro.model;
+
+/**
+ * Created by admin on 2018/6/20.
+ */
+public class User {
+    private String userName;
+    private  String password;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
